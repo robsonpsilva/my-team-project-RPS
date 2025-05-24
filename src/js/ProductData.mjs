8904b2c35350427d26fe83f211518cb005822d42
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-05-24 15:12:55 -0300

    modified:   src/js/ProductData.mjs

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -8,16 +8,16 @@ function convertToJson(res) {
 
 export default class ProductData {
   constructor(category) {
+    this.baseURL = import.meta.env.VITE_SERVER_URL;
     this.category = category;
-    this.path = `/json/${this.category}.json`;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+  async getData(category) {
+    const response = await fetch(`${this.baseURL}products/search/${category} `);
+    const data = await convertToJson(response);
+    return data.Result;
   }
   async findProductById(id) {
-    const products = await this.getData();
+    const products = await this.getData(this.category);
     return products.find((item) => item.Id === id);
   }
 }
