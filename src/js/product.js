158e7aaca95e7330cdae72b7465c6f6f1d061c84
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-05-24 15:36:54 -0300

    modified:   src/js/product.js

--- src/js/product.js
+++ src/js/product.js
@@ -2,31 +2,35 @@ import { getParam, getLocalStorage, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData("tents");
+
 const productID = getParam("product");
+const category = getParam("category");
+
+const dataSource = new ProductData(category);
 
 const productDetails = new ProductDetails(productID, dataSource);
 productDetails.init();
 
-function addProductToCart(product) {
-  const cartItems = getLocalStorage("so-cart") || [];
-  cartItems.push(product);
-  setLocalStorage("so-cart", cartItems);
-}
+// function addProductToCart(product) {
+//   const cartItems = getLocalStorage("so-cart") || [];
+//   cartItems.push(product);
+//   setLocalStorage("so-cart", cartItems);
+// }
 
 // add to cart button event handler
-async function addToCartHandler(e) {
-  const id = e.target?.dataset?.id;
-  if (!id) {
-    // console.error("No product ID found on button click!");
-    return;
-  }
+// async function addToCartHandler(e) {
+//   const id = e.target?.dataset?.id;
+//   if (!id) {
+//     // console.error("No product ID found on button click!");
+//     return;
+//   }
 
-  const selectedProduct = await dataSource.findProductById(id);
-  addProductToCart(selectedProduct);
-}
+//   const selectedProduct = await dataSource.findProductById(id);
+  
+//   productDetails.addProductToCart(selectedProduct);
+// }
 
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+// // add listener to Add to Cart button
+// document
+//   .getElementById("addToCart")
+//   .addEventListener("click", addToCartHandler);
